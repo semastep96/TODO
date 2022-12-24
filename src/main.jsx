ccabc3233b42ee53236b8d82436f29dea850d48b
semastep96 <semastep5@gmail.com>
2022-12-25 00:13:09 +0300

    add unique task check in fetch action

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import "./index.css";
 import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
--- src/store/thunks/fetchTasksAction.js
+++ src/store/thunks/fetchTasksAction.js
@@ -5,23 +5,31 @@ import {
   fetchTasksSuccess,
 } from "../actions/actions";
 
-const timeout = new Promise(res => setTimeout(res, 3000));
+const timeout = new Promise((res) => setTimeout(res, 3000));
 
-const fetchTasksAction = () => async (dispatch) => {
+const fetchTasksAction = () => async (dispatch, getState) => {
   try {
     dispatch(fetchTasks());
+
     await timeout;
+
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/todos?_limit=10"
     );
     const fetchedTasks = await res.json();
+
     const tasks = fetchedTasks.map((task, index) => ({
       id: task.title + index,
       text: task.title,
       done: false,
       category: index === 0 || index % 2 ? CATEGORIES.HIGH : CATEGORIES.LOW,
     }));
-    dispatch(fetchTasksSuccess(tasks));
+
+    const stateTodosId = getState().tasks.todos.map(todo => todo.id);
+    const isUniqueTask = (task) => !stateTodosId.includes(task.id);
+    const uniqueTasks = tasks.filter(isUniqueTask);
+
+    dispatch(fetchTasksSuccess(uniqueTasks));
   } catch (error) {
     dispatch(fetchTasksError(error.message));
   }
