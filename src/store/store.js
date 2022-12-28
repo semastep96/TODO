3f9ad1aa1b97d4f75f14c41e53b61f8fce7e54b5
semastep96 <semastep5@gmail.com>
2022-12-28 17:20:45 +0300

    redux logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import { logger } from "./middlewares/logger";
 import { reducer } from "./reducers/reducer";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export { store };
