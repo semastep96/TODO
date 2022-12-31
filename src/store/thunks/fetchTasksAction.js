ebfbae66d00e4e149c051d8c981e1f7da048f72b
semastep96 <semastep5@gmail.com>
2022-12-31 19:08:08 +0300

    thunk refactor

--- src/store/thunks/fetchTasksAction.js
+++ src/store/thunks/fetchTasksAction.js
@@ -7,25 +7,25 @@ import {
 
 const timeout = new Promise((res) => setTimeout(res, 3000));
 
+const API_URL = "https://jsonplaceholder.typicode.com/todos?_limit=10";
+
 const fetchTasksAction = () => async (dispatch, getState) => {
   try {
     dispatch(fetchTasks());
 
     await timeout;
 
-    const res = await fetch(
-      "https://jsonplaceholder.typicode.com/todos?_limit=10"
-    );
+    const res = await fetch(API_URL);
     const fetchedTasks = await res.json();
 
     const tasks = fetchedTasks.map((task, index) => ({
       id: task.title + index,
       text: task.title,
       done: false,
-      category: index === 0 || index % 2 ? CATEGORIES.HIGH : CATEGORIES.LOW,
+      category: index % 2 ? CATEGORIES.HIGH : CATEGORIES.LOW,
     }));
 
-    const stateTodosId = getState().tasks.todos.map(todo => todo.id);
+    const stateTodosId = getState().tasks.todos.map((todo) => todo.id);
     const isUniqueTask = (task) => !stateTodosId.includes(task.id);
     const uniqueTasks = tasks.filter(isUniqueTask);
 
